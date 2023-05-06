30d85d5ac155fecaebd4ce98c220c14bee9024d4
Ibrahim Tarek <ebrahimtarek555@gmail.com>
2023-05-06 22:25:06 +0300

    Adding Lecture 1 Labs.

---
+++ Lec1/Lec1_Lab1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//This is the main function
+int main(void)
+{
+	/*Printing Hello World on the screen*/
+	printf("Hello World!\n");
+	printf("I'm Ibrahim Tarek\nMy birthday is 28 Dec 2000\nI'll graduate from faculty of Engineering Mansoura University.\n");
+	
+	/*int x;
+	printf("Please enter the value: ");
+	scanf("%d", &x);
+	printf("The value You entered is %d", x);*/
+	
+	return 0;
+}
